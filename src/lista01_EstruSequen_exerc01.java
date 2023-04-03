a41226ded329415a9fa4c9c09ada7652c88e2791
Franciely Jamily <francielyj6@gmail.com>
2023-04-03 20:34:38 -0300

    Problema 01 - correcao da atividade.

---
+++ src/lista01_EstruSequen_exerc01.java
@@ -0,0 +1,6 @@
+public class lista01_EstruSequen_exerc01 {
+       //1.Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+       public static void main(String[] args) throws Exception {
+        System.out.println("Olá mundo!");
+     } 
+}
